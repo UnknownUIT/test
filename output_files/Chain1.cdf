9eea64169b5117c3482a4172598f3e47f34172d0
UnknownUIT <23521567@gmail.com>
2026-05-22 08:36:36 +0700

    up

---
+++ output_files/Chain1.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP2C35F672) Path("E:/CE433/Bai8/output_files/") File("Bai8.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ software/Bai8/obj/default/source.d
@@ -0,0 +1,23 @@
+obj/default/source.o: source.c ../Bai8_bsp/system.h ../Bai8_bsp/linker.h \
+  ../Bai8_bsp//drivers/inc/altera_avalon_dma_regs.h \
+  ../Bai8_bsp//HAL/inc/io.h ../Bai8_bsp//HAL/inc/alt_types.h \
+  ../Bai8_bsp//HAL/inc/sys/alt_irq.h ../Bai8_bsp//HAL/inc/nios2.h \
+  ../Bai8_bsp//HAL/inc/alt_types.h ../Bai8_bsp/system.h
+
+../Bai8_bsp/system.h:
+
+../Bai8_bsp/linker.h:
+
+../Bai8_bsp//drivers/inc/altera_avalon_dma_regs.h:
+
+../Bai8_bsp//HAL/inc/io.h:
+
+../Bai8_bsp//HAL/inc/alt_types.h:
+
+../Bai8_bsp//HAL/inc/sys/alt_irq.h:
+
+../Bai8_bsp//HAL/inc/nios2.h:
+
+../Bai8_bsp//HAL/inc/alt_types.h:
+
+../Bai8_bsp/system.h:
